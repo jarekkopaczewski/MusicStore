4cb4d0594db07695848ce0a32735b4e2375748fa
jarekkopaczewski <m4n0fsh4dow@gmail.com>
2022-01-21 10:16:19 +0100

    -store/warehouse serialized class

---
+++ app/src/main/java/com/example/musicstore/ProductSM.kt
@@ -0,0 +1,10 @@
+package com.example.musicstore
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductSM(
+    @SerializedName("KOD_KRESKOWY") val kod_kreskowy: String,
+    @SerializedName("NAZWA_PRODUKTU") val nazwa: String,
+    @SerializedName("DOSTEPNOSC") val dostepnosc: String,
+    @SerializedName("ILOSC") val cena: Int
+)
